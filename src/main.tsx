20e1f9686d4901355f51c531b6bb612934bee358
chann44 <44chansw@gmail.com>
2022-11-01 12:21:53 +0530

    added layout colors

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <div className="bg-primaryDark">
+    <div className="bg-primaryDark min-h-screen max-w-7xl border mx-auto">
+      <App />
+    </div>
+    </div>
+ </React.StrictMode>
+);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,11 +7,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primaryDark": "",
-        "secondaryDark": "",
-        "thirdDark": "",
-        "PrimaryBlue": "",
-        "faddedBlue": ""
+        "primaryDark": "#121212",
+        "secondaryDark": "#191919",
+        "thirdDark": "#1C1C1E",
+        "forDark": "",
+        "PrimaryBlue": "#5856D6",
+        "purple": "#6739B7",
+        "faddedBlue": "rgba(103, 57, 183, 0.1)",
+        "faddedDark":"#1A1A1A",
+        "text-faded": "#78778B",
+        "text": "rgba(255, 255, 255, 0.87)",
+        "red": "#E95065",
+        "green": "#34C759"
       }
     },
   },
